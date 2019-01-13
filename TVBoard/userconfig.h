919c1d0df790ca352c1d7c6de58d3c99483e3b65
J3n50m4t <j3n50m4t@j3n50m4t.com>
2019-01-13 17:05:31 +0100

    Added userconfig

---
+++ TVBoard/userconfig.h
@@ -0,0 +1,6 @@
+const char* ssid = "name"; 
+const char* password = "password";
+const char* mqtt_server = "ip"; 
+const uint16_t mqtt_port = 1883;
+const char* mqtt_user = "username";
+const char* mqtt_password = "password";
